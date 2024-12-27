f5577bd1eba3833cc03ff04f5f8905e5a5d31ec1
berhan <berhan5378@gmail.com>
2024-12-27 22:33:31 +0300

    10-1014_Best_Sightseeing_Pair

---
+++ 10-1014_Best_Sightseeing_Pair.php
@@ -0,0 +1,19 @@
+<?php
+
+class Solution {
+    /**
+      * @param Integer[] $values
+      * @return Integer
+      */
+    function maxScoreSightseeingPair($values) {
+      $max = 0;
+      $score = $values[0];
+      $length = count($values);
+      for ($i = 1; $i < $length; ++$i) {
+        --$score;//values[i]+i-j;
+        if ($score + $values[$i] > $max) $max = $score + $values[$i];
+        if ($values[$i] > $score) $score = $values[$i];
+      }
+      return $max;
+    }
+  }
